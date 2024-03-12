b2920b8b37ddd17bd0c9d69e113d52a75ab87f85
marynahaida <marinkagaida.15@gmail.com>
2024-03-12 10:52:34 +0100

    Add new System.out.println

--- Src/GitHomeworkLesson4.java
+++ Src/GitHomeworkLesson4.java
@@ -1,5 +1,6 @@
 public class GitHomeworkLesson4 {
     public static void main(String[] args){
         System.out.println("Hallo, Git");
+        System.out.println("Second commit to Git");
     }
 }
